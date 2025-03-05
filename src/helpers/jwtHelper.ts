45826b92caf6792e0063869d2aad5c59d2d86245
mdsharifulislam-r <masom3456@gmail.com>
2025-03-05 16:14:26 +0600

    ite

--- src/helpers/jwtHelper.ts
+++ src/helpers/jwtHelper.ts
@@ -1,7 +1,7 @@
 import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 
 const createToken = (payload: object, secret: Secret, expireTime: string) => {
-  return jwt.sign(payload, secret, { expiresIn: expireTime });
+  return jwt.sign(payload, secret, { expiresIn: expireTime })
 };
 
 const verifyToken = (token: string, secret: Secret) => {
